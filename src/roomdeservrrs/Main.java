e4ea3e55faf24d68fa88ce8326549df9de9f7c1e
Ervhyne(Catcher) <119420792+Ervhyne@users.noreply.github.com>
2023-05-16 22:33:04 +0800

    Add files via upload

---
+++ src/roomdeservrrs/Main.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package roomdeservrrs;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author velas
+ */
+public class Main extends Application {
+    
+    private double x = 0;
+    private double y = 0;
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/login/LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        root.setOnMousePressed((javafx.scene.input.MouseEvent event) ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        
+        root.setOnMouseDragged((javafx.scene.input.MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+            
+            stage.setOpacity(.8);
+        });
+        
+        root.setOnMouseReleased((javafx.scene.input.MouseEvent event) ->{
+            stage.setOpacity(1);
+        });
+         
+        Image icon = new Image(getClass().getResourceAsStream("/img/appicon.png"));
+        stage.getIcons().add(icon);
+
+        
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
